5a462b2ace618d3d8ac7cf81f793b7c0a2bd64bf
Jatinchawla30 <JIGNESH.OLA77@GMAIL.COM>
2025-09-05 17:04:59 +0530

    Update tailwind.config.js

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",   // ✅ All React files
-    "./public/index.html"           // ✅ Also scan index.html (safety)
+    "./src/**/*.{js,jsx,ts,tsx}",   // Scan all React files
+    "./public/index.html"           // Also scan index.html
   ],
   theme: {
     extend: {},
