8293fc4c5b3c2c6533f3e454bf126af60623fbae
Jatinchawla30 <JIGNESH.OLA77@GMAIL.COM>
2025-09-07 22:46:38 +0530

    Create Cylinders.js

---
+++ src/components/cylinders/Cylinders.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Cylinders() {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold">Cylinders</h1>
+      <p className="text-gray-600 mt-4">Manage your cylinders here.</p>
+    </div>
+  );
+}
