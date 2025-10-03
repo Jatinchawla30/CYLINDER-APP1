f3c1de278eef579af356a3c6b91b6c12ea36ea7c
Jatinchawla30 <JIGNESH.OLA77@GMAIL.COM>
2025-10-03 13:57:04 +0530

    Update ProtectedRoute.js

--- src/components/auth/ProtectedRoute.js
+++ src/components/auth/ProtectedRoute.js
@@ -1,15 +1,14 @@
-import React from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
 
 const ProtectedRoute = ({ children }) => {
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
 
-  if (!user) {
-    return <Navigate to="/login" replace />;
+  if (loading) {
+    return <div>Loading...</div>;
   }
 
-  return children;
+  return user ? children : <Navigate to="/login" replace />;
 };
 
 export default ProtectedRoute;
